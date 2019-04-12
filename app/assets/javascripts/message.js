da29d369684c50c4d081c36a6961173e85b88a71
ankayuki <takahashiyuki@takahashiyukinoMacBook-puro.local>
2019-04-12 18:54:19 +0900

    Change reset method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,8 +39,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__new-message__input-box__text').val('');
-      $('.form__new-message__input-box__image__file').val('');
+      $('#new_message')[0].reset();
       $('.form__new-message__submit-button').prop('disabled', false);
       var $messages = $('.messages');
       $messages.animate({
