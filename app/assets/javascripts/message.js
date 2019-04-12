686f62780779874a513c33e2cc886e9cde39715e
ankayuki <takahashiyuki@takahashiyukinoMacBook-puro.local>
2019-04-12 16:46:20 +0900

    HTML of API
    This HTML is inserted instead of view file.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,3 +3,21 @@
     if (message.image) {
       insertImage = `<img src="${message.image}">`;
     };
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    <div class="lower-message__image">
+                      ${insertImage}
+                    </div>
+                  </div>
+                </div>`;
